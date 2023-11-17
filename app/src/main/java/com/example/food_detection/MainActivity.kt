335c4777b67fc921bd72e92dd7ad1166614119a9
Helen259 <fedorovalena2002@yandex.ru>
2023-11-17 04:38:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/food_detection/MainActivity.kt
@@ -0,0 +1,356 @@
+package com.example.food_detection
+
+import ai.onnxruntime.OnnxTensor
+import ai.onnxruntime.OrtEnvironment
+import ai.onnxruntime.OrtSession
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.*
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.detector.Detection
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.max
+import kotlin.math.min
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    companion object {
+        const val TAG = "ONNX - ODT"
+        const val REQUEST_IMAGE_CAPTURE: Int = 1
+        private const val MAX_FONT_SIZE = 96F
+    }
+
+    private val dataProcess = DataProcess(context = this)
+    private var calories = Calories()
+
+    private lateinit var ortEnvironment: OrtEnvironment
+    private lateinit var session: OrtSession
+    private lateinit var classes: Array<String>
+
+    private lateinit var captureImageFab: Button
+    private lateinit var inputImageView: ImageView
+    private lateinit var imgSampleOne: ImageView
+    private lateinit var imgSampleTwo: ImageView
+    private lateinit var imgSampleThree: ImageView
+    private lateinit var tvPlaceholder: TextView
+    private lateinit var currentPhotoPath: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        captureImageFab = findViewById(R.id.captureImageFab)
+        inputImageView = findViewById(R.id.imageView)
+        imgSampleOne = findViewById(R.id.imgSampleOne)
+        imgSampleTwo = findViewById(R.id.imgSampleTwo)
+        imgSampleThree = findViewById(R.id.imgSampleThree)
+        tvPlaceholder = findViewById(R.id.tvPlaceholder)
+
+        captureImageFab.setOnClickListener(this)
+        imgSampleOne.setOnClickListener(this)
+        imgSampleTwo.setOnClickListener(this)
+        imgSampleThree.setOnClickListener(this)
+
+        load()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE &&
+            resultCode == Activity.RESULT_OK
+        ) {
+            setViewAndDetect(getCapturedImage())
+        }
+    }
+
+    /**
+     * onClick(v: View?)
+     *      Detect touches on the UI components
+     */
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.captureImageFab -> {
+                try {
+                    dispatchTakePictureIntent()
+                } catch (e: ActivityNotFoundException) {
+                    Log.e(TAG, e.message.toString())
+                }
+            }
+            R.id.imgSampleOne -> {
+                setViewAndDetect(getSampleImage(R.drawable.banana_orange))
+            }
+            R.id.imgSampleTwo -> {
+                setViewAndDetect(getSampleImage(R.drawable.pizza))
+            }
+            R.id.imgSampleThree -> {
+                setViewAndDetect(getSampleImage(R.drawable.cucumber_tomato))
+            }
+        }
+    }
+
+    private fun runObjectDetection(bitmap: Bitmap) {
+        val scaledBitmap = dataProcess.bitmapToScaledBitmap(bitmap)
+
+        val floatBuffer = dataProcess.bitmapToFloatBuffer(scaledBitmap)
+        val inputName = session.inputNames.iterator().next()
+        val shape = longArrayOf(
+            DataProcess.BATCH_SIZE.toLong(),
+            DataProcess.PIXEL_SIZE.toLong(),
+            DataProcess.INPUT_SIZE.toLong(),
+            DataProcess.INPUT_SIZE.toLong()
+        )
+
+        // создаем Input для YOLO из изображения
+        val inputTensor = OnnxTensor.createTensor(ortEnvironment, floatBuffer, shape)
+        val resultTensor = session.run(Collections.singletonMap(inputName, inputTensor))
+        val outputs = resultTensor.get(0).value as Array<*>
+        val results = dataProcess.outputsToNPMSPredictions(outputs) // предсказания модели
+
+        val resultLabels = mutableListOf<String>()
+
+        // конвертируем предсказания в формат DetectionResult
+        val resultToDisplay = results.map {
+            val class_name = classes[it.classIndex]
+
+            val text = class_name
+            val cal = "${class_name}: ${calories.calories[class_name]} kcal(100 g)"
+            resultLabels.add(cal)
+
+            // Возвращаем изображение в исходные размеры
+            val scaleX = bitmap.width / DataProcess.INPUT_SIZE.toFloat()
+            val scaleY = scaleX * 9f / 16f
+            val realY = bitmap.width * 9f / 16f
+            val diffY = realY - bitmap.height
+
+            val rectF = it.rectF
+            val left = rectF.left * scaleX
+            val right = rectF.right * scaleX
+            val top = rectF.top * scaleY - (diffY / 2f)
+            val bottom = rectF.bottom * scaleY - (diffY / 2f)
+
+            val newRectF = RectF(left, top, right, bottom)
+
+            DetectionResult(newRectF, text)
+        }
+
+        val textViewLabels = findViewById<TextView>(R.id.tvLabels)
+        val imgWithResult = drawDetectionResult(bitmap, resultToDisplay)
+        runOnUiThread {
+            inputImageView.setImageBitmap(imgWithResult)
+            textViewLabels.text = resultLabels.joinToString()
+        }
+    }
+
+    /**
+     * setViewAndDetect(bitmap: Bitmap)
+     *      Set image to view and call object detection
+     */
+    private fun setViewAndDetect(bitmap: Bitmap) {
+        // Display capture image
+        inputImageView.setImageBitmap(bitmap)
+        tvPlaceholder.visibility = View.INVISIBLE
+
+        // Run ODT and display result
+        // Note that we run this in the background thread to avoid blocking the app UI because
+        // ONNX object detection is a synchronised process.
+        lifecycleScope.launch(Dispatchers.Default) { runObjectDetection(bitmap) }
+    }
+
+    /**
+     * getCapturedImage():
+     *      Decodes and crops the captured image from camera.
+     */
+    private fun getCapturedImage(): Bitmap {
+        // Get the dimensions of the View
+        val targetW: Int = inputImageView.width
+        val targetH: Int = inputImageView.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            BitmapFactory.decodeFile(currentPhotoPath, this)
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+            val scaleFactor: Int = max(1, min(photoW / targetW, photoH / targetH))
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inMutable = true
+        }
+        val exifInterface = ExifInterface(currentPhotoPath)
+        val orientation = exifInterface.getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_UNDEFINED
+        )
+
+        val bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> {
+                rotateImage(bitmap, 90f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_180 -> {
+                rotateImage(bitmap, 180f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_270 -> {
+                rotateImage(bitmap, 270f)
+            }
+            else -> {
+                bitmap
+            }
+        }
+    }
+
+    /**
+     * getSampleImage():
+     *      Get image form drawable and convert to bitmap.
+     */
+    private fun getSampleImage(drawable: Int): Bitmap {
+        return BitmapFactory.decodeResource(resources, drawable, BitmapFactory.Options().apply {
+            inMutable = true
+        })
+    }
+
+    /**
+     * rotateImage():
+     *     Decodes and crops the captured image from camera.
+     */
+    private fun rotateImage(source: Bitmap, angle: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+        return Bitmap.createBitmap(
+            source, 0, 0, source.width, source.height,
+            matrix, true
+        )
+    }
+
+    /**
+     * createImageFile():
+     *     Generates a temporary image file for the Camera app to write to.
+     */
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    /**
+     * dispatchTakePictureIntent():
+     *     Start the Camera app to take a photo.
+     */
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (e: IOException) {
+                    Log.e(TAG, e.message.toString())
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "org.tensorflow.codelabs.objectdetection.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    /**
+     * drawDetectionResult(bitmap: Bitmap, detectionResults: List<DetectionResult>
+     *      Draw a box around each objects and show the object's name.
+     */
+    private fun drawDetectionResult(
+        bitmap: Bitmap,
+        detectionResults: List<DetectionResult>
+    ): Bitmap {
+        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(outputBitmap)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+        detectionResults.forEach {
+            // draw bounding box
+            pen.color = Color.RED
+            pen.strokeWidth = 8F
+            pen.style = Paint.Style.STROKE
+            val box = it.boundingBox
+            canvas.drawRect(box, pen)
+
+            val tagSize = Rect(0, 0, 0, 0)
+
+            // calculate the right font size
+            pen.style = Paint.Style.FILL_AND_STROKE
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 2F
+
+            pen.textSize = MAX_FONT_SIZE
+            pen.getTextBounds(it.text, 0, it.text.length, tagSize)
+            val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+            // adjust the font size so texts are inside the bounding box
+            if (fontSize < pen.textSize) pen.textSize = fontSize
+
+            var margin = (box.width() - tagSize.width()) / 2.0F
+            if (margin < 0F) margin = 0F
+            canvas.drawText(
+                it.text, box.left + margin,
+                box.top + tagSize.height().times(1F), pen
+            )
+        }
+        return outputBitmap
+    }
+
+    private fun load() { // загружаем лейблы и модель
+        dataProcess.loadModel()
+        dataProcess.loadLabel()
+
+        ortEnvironment = OrtEnvironment.getEnvironment()
+        session = ortEnvironment.createSession(
+            this.filesDir.absolutePath.toString() + "/" + DataProcess.FILE_NAME,
+            OrtSession.SessionOptions()
+        )
+
+        this.classes = dataProcess.classes
+    }
+}
